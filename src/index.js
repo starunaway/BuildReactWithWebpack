86f57e71ae4a00d1e52a3fccd510f826502e16d3
keeping <starunaway@gmail.com>
2020-04-28 23:31:36 +0800

    为什么使用webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+function component() {
+  var element = document.createElement('div');
+  element.innerHTML = 'hello webpack';
+  element.innerHTML += moment().format('YYYY-MM-DD HH:mm:ss');
+  return element;
+}
+document.body.appendChild(component());
