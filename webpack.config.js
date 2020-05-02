a2ba52eedf707e6d83acddd7fc2c6d7cccb0990f
keeping <starunaway@gmail.com>
2020-05-02 18:29:22 +0800

    开发时加入代码原始位置的映射

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     app: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
