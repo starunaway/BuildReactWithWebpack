db193eb67508689472cc690ea12cd1c4e6c583c2
keeping <starunaway@gmail.com>
2020-05-02 18:14:42 +0800

    使用多入口

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import moment from 'moment';
+import printMe from './print';
 import './style.less';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
   element.innerHTML = 'hello webpack';
   element.innerHTML += moment().format('YYYY-MM-DD HH:mm:ss');
 
   element.classList.add('time');
+  element.appendChild(btn);
+
   return element;
 }
 document.body.appendChild(component());
