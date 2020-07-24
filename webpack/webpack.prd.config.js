0cd18125f9af0674ec4d5e56c0368a1d0bc9d1b4
keeping <starunaway@gmail.com>
2020-07-24 21:53:52 +0800

    feat: 🎸 update webpack config: postcss loader (sth err)

--- webpack/webpack.prd.config.js
+++ webpack/webpack.prd.config.js
@@ -11,7 +11,21 @@ module.exports = {
           MiniCssExtractPlugin.loader,
           // 将css | less 文件中的样式插入到js文件中，里面的内容是样式字符串
           'css-loader',
-          //   less-loader -> 将less文件转换成css文件
+          //   less-loader -> 将less文件转换成css文件,需要提前安装less
+          //   'less-loader',
+          // 帮postcss找到package.json中的browserlist配置，通过配置加载指定的css兼容样式
+          // 默认生产环境，是nodejs的运行环境，和webpack的mode无关
+          // process.env.NODE_ENV="development"
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                //postcss 插件
+                require('postcss-preset-env')(),
+              ],
+            },
+          },
         ],
       },
     ],
